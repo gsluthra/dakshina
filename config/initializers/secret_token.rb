4b27ef01c18a559048c522f731be9579728fc5ac
Gurpreet Luthra <gsluthra@gmail.com>
2013-09-21 23:12:34 +0530

    Basic Rails project without any TESTS and ACTIVE record. Not web tested.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dakshina::Application.config.secret_token = 'b9a99919f21329044301d790beed8f0fc0c6401fb8499bc1b5beb97dbedd01cc76d9ed20ade177aa053ef44fa809fc7e256e48a2ac0cee4a1aa2da8fa493d278'
