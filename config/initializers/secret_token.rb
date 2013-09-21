e5751c5a3e6ac6fb98820fcde2213c0d10489291
Gurpreet Luthra <gsluthra@gmail.com>
2013-09-21 22:33:45 +0530

    Reset everything

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dakshina::Application.config.secret_token = '1d4396d29d9cc318b33f25c01d2dec218e14900346d8a56bd32439a98ff05c689de13f2f9d512d9ed2faa8cdfaa04e9bcf2fed2dcb41d1c985d92dbadabea4c6'
