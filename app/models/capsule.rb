47e9dbc11872ecf7b64f94c7af213d9c3cb95555
Gurpreet Luthra <gsluthra@gmail.com>
2013-12-31 15:56:50 +0530

    Added unit tests for capsule model object

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -1,6 +1,6 @@
 class Capsule < ActiveRecord::Base
 
-  validates :title, presence: true, length: { minimum: 3 }
+  validates :title, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
 
 end
