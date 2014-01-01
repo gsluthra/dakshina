9df14a50c46b22ee1161cbb3ae910af7578aaeb5
Gurpreet Luthra <gsluthra@gmail.com>
2014-01-01 16:08:27 +0530

    Removed the presence test since length test does that automatically

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -1,6 +1,6 @@
 class Capsule < ActiveRecord::Base
 
   validates :title, length: { minimum: 3 }
-  validates :description, presence: true, length: { minimum: 5 }
+  validates :description, length: { minimum: 5 }
 
 end
