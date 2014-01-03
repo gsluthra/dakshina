f63d54627b202eccbe2386e389813fe0f5f00289
Gurpreet Luthra <gsluthra@gmail.com>
2014-01-03 21:28:10 +0530

    Enhanced capsule factory to contain all attributes

--- spec/factories/capsules.rb
+++ spec/factories/capsules.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :capsule do |f|
-    f.title "TDD-Part1"
-    f.description "TDD Introduction part 1"
+    f.title 'TDD-Part1'
+    f.description 'TDD Introduction part 1'
+    f.study_text 'Some study text with hyperlink: http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html'
+    f.assignment_instructions 'Submit by tomorrow'
+    f.guidelines_for_evaluators 'Ensure tests are clean and negative scenarios are covered'
   end
 end
