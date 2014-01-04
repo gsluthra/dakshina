12eabc3953eb2e63eb2e88f9b72bee7bfaeaf3c3
Gurpreet Luthra <gsluthra@gmail.com>
2014-01-04 11:00:10 +0530

    Renamed to capsule_factory. Easy to search in IDE.

--- spec/factories/capsule_factory.rb
+++ spec/factories/capsule_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :capsule do |f|
-    f.title 'TDD-Part1'
-    f.description 'TDD Introduction part 1'
+    f.title 'TDD Intro'
+    f.description 'TDD Introduction for students'
     f.study_text 'Some study text with hyperlink: http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html'
     f.assignment_instructions 'Submit by tomorrow'
     f.guidelines_for_evaluators 'Ensure tests are clean and negative scenarios are covered'
