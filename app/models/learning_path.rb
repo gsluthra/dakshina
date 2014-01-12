4e64c3b67266d004ccf85b667a2f3106d10832fa
Gurpreet Luthra <gsluthra@gmail.com>
2014-01-12 21:21:08 +0530

    Added capsule count in Learning Path page. Added capsule details display to Learning Path View Page

--- app/models/learning_path.rb
+++ app/models/learning_path.rb
@@ -1,7 +1,7 @@
 class LearningPath < ActiveRecord::Base
   validates :name, length: { minimum: 2 }, uniqueness: true
 
-  has_many :learning_path_elements
+  has_many :learning_path_elements, :order => 'position'
   has_many :capsules, :through => :learning_path_elements
 
 end
