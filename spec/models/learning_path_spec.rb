d4103c8ffa7e3e4ea9d74a36984c38f84e0c1d84
gsluthra <gluthra@thoughtworks.com>
2014-02-02 16:46:45 +0530

    Added unit tests for learning path model

--- spec/models/learning_path_spec.rb
+++ spec/models/learning_path_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe LearningPath do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(create(:dev_track)).to be_valid
+  end
+
+  it 'has a valid factory with invalid learning path' do
+    expect(build(:nil_name_track)).to_not be_valid
+  end
+
+  it 'is invalid if name is not passed' do
+    expect(build(:dev_track, name: nil)).to_not be_valid
+  end
+
+  it 'is invalid if name already exists on another learning path' do
+    some_title = 'QA Track'
+    expect(create(:dev_track, name: some_title)).to be_valid
+    expect(build(:dev_track, name: some_title)).to_not be_valid
+  end
 end
