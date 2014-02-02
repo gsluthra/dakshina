ed36cfabb899b5fac076de98fc2624d1372a9f19
gsluthra <gluthra@thoughtworks.com>
2014-02-02 16:23:16 +0530

    PUT #update tests

--- spec/controllers/learning_paths_controller_spec.rb
+++ spec/controllers/learning_paths_controller_spec.rb
@@ -68,6 +68,54 @@ describe LearningPathsController do
 
   end
 
+  describe 'PUT #update' do
+
+    let (:saved_path) {create(:dev_track)}
+
+    context 'Valid attributes' do
+      it 'changes the learning path attributes and redirects to updated learning path' do
+
+        NEW_NAME = 'Updated Name'
+        NEW_DESCRIPTION = 'Updated description'
+        NEW_OBJECTIVE = 'New objective'
+
+        put :update, id: saved_path.id,
+            learning_path: attributes_for(:dev_track, name: NEW_NAME, description: NEW_DESCRIPTION, objective: NEW_OBJECTIVE)
+
+        saved_path.reload
+        expect(saved_path.name).to eq(NEW_NAME)
+        expect(saved_path.description).to eq(NEW_DESCRIPTION)
+        expect(saved_path.objective).to eq(NEW_OBJECTIVE)
+
+        expect(response).to redirect_to saved_path
+
+      end
+
+    end
+
+    context 'InValid attributes' do
+      it 'does not update the path and re-renders the edit method' do
+
+        put :update, id: saved_path.id,
+            learning_path: attributes_for(:nil_name_track)
+
+        saved_path.reload
+        original_dev_path = build(:dev_track)
+        expect(saved_path.name).to eq(original_dev_path.name)
+        expect(saved_path.description).to eq(original_dev_path.description)
+        expect(saved_path.objective).to eq(original_dev_path.objective)
+
+        expect(response).to render_template :edit
+
+      end
+
+    end
+
+
+  end
+
+
+
 
 
 end
