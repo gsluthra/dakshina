7b02758532eb19db0fcc3f5b92b2341f9cc72484
gsluthra <gluthra@thoughtworks.com>
2014-02-02 19:54:34 +0530

    Modified modal to show a checkbox for selecting capsules. Still need to perform the submit related code

--- app/controllers/learning_elements_controller.rb
+++ app/controllers/learning_elements_controller.rb
@@ -2,9 +2,9 @@ class LearningElementsController < ApplicationController
 
   def potential_capsules_for_path
 
-    learning_path = LearningPath.find(params[:path_id])
-    if learning_path.capsules.count > 0
-      @potential_capsules = Capsule.where('id not in (?)', learning_path.capsules.map(&:path_id))
+    @learning_path = LearningPath.find(params[:path_id])
+    if @learning_path.capsules.count > 0
+      @potential_capsules = Capsule.where('id not in (?)', @learning_path.capsules.map(&:path_id))
     else
       puts 'Returning all capsules'
       @potential_capsules = Capsule.all
